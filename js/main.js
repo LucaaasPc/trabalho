93b56a008d07ef65e44d43eda33d5e0c00b1b59f
Anderson Leite <anderson07102010@hotmail.com>
2022-11-22 22:52:41 -0300

    Add more js coding

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 // Abre e fecha o menu quando clicar no icone
-
 const nav = document.querySelector('header nav');
 const toggle = document.querySelectorAll('nav .toggle');
 
-console.log(toggle)
 for (const element of toggle){
   element.addEventListener('click', ()=>{
     nav.classList.toggle('show')
   })
 }
+
+// Faz o corte do video após 16 segundos
+var video = document.querySelector('video');
+var cutVideo = false;
+    video.ontimeupdate = ()=>{
+        if (video.currentTime >= 16 && cutVideo == false){
+            cutVideo = true;
+            video.currentTime = 33;
+            video.play();
+        } 
+    };
+
+// Adiciona autoplay para o video
+window.addEventListener('load', ()=>{
+  const video = document.querySelector('video');
+  video.autoplay = true;
+  video.muted = true;
+  video.load();
+
+});
+
+// Inicia Audio Fallen
+const button = document.querySelector('button')
+button.addEventListener('click', function(){
+const audio = document.querySelector('audio')
+audio.play()
+}
+)
